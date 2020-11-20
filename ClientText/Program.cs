ec4e5fb838d3c1c95986291b3d4e281c6e5d380d
nerstak <roy.karst@gmail.com>
2020-11-20 19:31:40 +0100

    Initalising projects

---
+++ ClientText/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
