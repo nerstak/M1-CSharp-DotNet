70e1ae6634aae403810d8025389ea7a1ee5a0fc9
nerstak <roy.karst@gmail.com>
2020-12-03 18:34:40 +0100

    Client view basis

---
+++ ClientText/src/view/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ClientText.controller;
+
+namespace ClientText.view
+{
+    public class Client
+    {
+        private string hostname;
+        private int port;
+
+        public Client(string hostname, int port)
+        {
+            this.hostname = hostname;
+            this.port = port;
+        }
+
+        public void Start()
+        {
+            AbstractAction.Start(hostname,port);
+            Console.Out.WriteLine("Connection established");
+
+            MainLoop();
+        }
+
+        private void MainLoop()
+        {
+            UserConnection();
+        }
+
+        private void UserConnection()
+        {
+            bool flag = false;
+            do
+            {
+                Console.Out.WriteLine("Username: ");
+                var username = Console.ReadLine();
+                Console.Out.WriteLine("Password: ");
+                var password = Console.ReadLine();
+
+                flag = Login.ConnectUser(username, password);
+                if (!flag)
+                {
+                    Console.Out.WriteLine("Wrong credentials");
+                }
+            } while (!flag);
+            Console.Out.WriteLine("You have been connected!");
+        }
+    }
+}
