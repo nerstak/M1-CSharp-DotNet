2cafc52c0eb831650e274906b73c56208f5149d5
nerstak <roy.karst@gmail.com>
2020-12-10 18:35:18 +0100

    Windows with correct names

---
+++ ClientGUI/ClientGui/ChatWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ClientGUI
+{
+    public partial class ChatWindow : Window
+    {
+        public ChatWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
